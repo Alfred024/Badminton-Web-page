42029a5258e0461629212fdd35bee2971d38669b
unknown <21030761@itcelaya.edu.mx>
2024-10-16 20:44:18 -0600

    Config folder created

--- config/config.php
+++ config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+    // DATABASE 
     define("SGDB", 'mysql');
     define("DBHOST", 'localhost');
     define("DBNAME", 'badminton_profe');
@@ -7,4 +8,9 @@
     define("DBPASS", '123');
     define("DBPORT", '3306'); 
     
+    //SMTP
+    define("SMTP_HOST", 'smtp.gmail.com');
+    define("SMTP_PORT", '465');
+    define("SMTP_EMAIL", '21030761@itcelaya.edu.mx');
+    define("SMTP_PSWD", 'lpqw frdd bgvu xoiw');
 ?>
