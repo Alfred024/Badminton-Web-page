d8ee95a75ec8365119acf800cbc03d5e9770998a
unknown <21030761@itcelaya.edu.mx>
2024-10-29 10:40:46 -0600

    Update

--- classes/class_role.php
+++ classes/class_role.php
@@ -8,27 +8,41 @@
 
         function action($action_case){
             switch ($action_case) {
+                case 'formEdit':
                 case 'formNew':
-                    // $methodForm = $_REQUEST['name']; // insert/update
+
+                    if ( $action_case == 'formNew' ){
+                        $button_label = 'Crear';
+                        $method_name = 'create';
+                        $rol = '';
+                    }else{
+                        $button_label = 'Actualizar';
+                        $method_name = 'update';
+                        $id = $_REQUEST['id_rol'];
+                        $rol = $_REQUEST['role'];
+                        // echo 'VOY A EDITAR';
+                    }
 
                     $form = '
                     <div class="container">
                         <div class="row">
                             <div class="col-md-12">
-                                <form method="post" >
+                                <form method="POST" >
+                                '. (isset($id) ? "<input type='hidden' name='id_rol' value=".$id.">" : "") .'
+
                                     <div class="form-group">
                                         
                                         <label for="role_name">
                                             Rol: 
                                         </label>
-                                        <input id="role_name" name="rol" type="text" class="form-control"/>
+                                        <input id="role_name" name="rol" type="text" class="form-control" value="'.$rol.'"/>
                                     </div>
                                     <div class="d-flex justify-content-end mt-4">
                                         <button type="submit" class="btn bg-blue text-white">
-                                            Crear/Actualizar
+                                            '.$button_label.'
                                         </button>
                                     </div>
-                                    <input type="hidden" value="create" name="action">
+                                    <input type="hidden" value="'.$method_name.'" name="action">
                                 </form>
                             </div>
                         </div>
@@ -69,13 +83,15 @@
 
         function update(){
             $rol = $_POST['rol'];
+            $id_rol = $_POST['id_rol'];
+            
             $update_rol_query = '
-                UPDATE rol ( rol ) 
-                VALUES ( :rol );';
-
-            $params = [':rol' => $rol,];
+                UPDATE rol SET rol = :rol 
+                WHERE id_rol = :id_rol;';
+            $params = [':rol' => $rol, ':id_rol' => $id_rol];
 
             $this->do_query($update_rol_query, $params);
+            $this->read();
         }
 
 
@@ -111,9 +127,11 @@
                         <td class='text-center'> ".$register['rol']." </td>
                         <td class='text-center'>
                             <div class='d-flex justify-content-center gap-2'>
-                                <form class='btn btn-primary'>
+                                <form method='POST' class='btn btn-primary'>
                                     <input type='image' class='svg-white' style='width: 25px;' src='../assets/icons/edit-button.svg' alt='Edit icon' srcset=''>
-                                   
+                                    <input type='hidden' name='action' value='formEdit'>
+                                    <input type='hidden' name='role' value=".$register['rol'].">
+                                    <input type='hidden' name='id_rol' value=".$register['id_rol'].">
                                 </form>
                                 <form method='POST' class='btn btn-danger'>
                                     <input type='image' class='svg-white' style='width: 30px;' src='../assets/icons/delete.svg' alt='Delete icon' srcset=''>
