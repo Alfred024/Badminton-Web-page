2e96bc81beebc9330925c3253bfc695de6cda1ca
unknown <21030761@itcelaya.edu.mx>
2024-10-29 10:13:45 -0600

    Action of delete and create role completed

--- admin/roles.php
+++ admin/roles.php
@@ -11,13 +11,6 @@
     <h1 class="text-center mb-4 color-dark-blue">Pestaña de roles</h1>
 
     <div class="container mt-4">
-        <div class="d-flex justify-content-end mb-3">
-            
-            <button class="btn btn-success btn-sm" title="Agregar nuevo rol">
-                <i class="bi bi-plus-lg"></i> Agregar Rol
-            </button>
-        </div>
-
         <?php 
             include '../classes/class_role.php';
         ?>
--- classes/class_role.php
+++ classes/class_role.php
@@ -8,14 +8,42 @@
 
         function action($action_case){
             switch ($action_case) {
+                case 'formNew':
+                    // $methodForm = $_REQUEST['name']; // insert/update
+
+                    $form = '
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <form method="post" >
+                                    <div class="form-group">
+                                        
+                                        <label for="role_name">
+                                            Rol: 
+                                        </label>
+                                        <input id="role_name" name="rol" type="text" class="form-control"/>
+                                    </div>
+                                    <div class="d-flex justify-content-end mt-4">
+                                        <button type="submit" class="btn bg-blue text-white">
+                                            Crear/Actualizar
+                                        </button>
+                                    </div>
+                                    <input type="hidden" value="create" name="action">
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    ';
+                    echo $form;
+                break;
                 case 'create':
                     $this->create();
                     break;
-                case 'read':
-                    $this->read();
-                break;
                 case 'update':
                     $this->update();
+                    break;
+                case 'read':
+                    $this->read();
                 break;
                 case 'delete':
                     $this->delete();
@@ -27,23 +55,44 @@
         }
 
         function create(){
+            // echo ' VAMOS A HACER UN INSERT ';
             $rol = $_POST['rol'];
             $insert_rol_query = '
                 INSERT INTO rol ( rol ) 
                 VALUES ( :rol );';
+            
+            $params = [':rol' => $rol,];
+            
+            $this->do_query($insert_rol_query, $params);
+            $this->read();
+        }
+
+        function update(){
+            $rol = $_POST['rol'];
+            $update_rol_query = '
+                UPDATE rol ( rol ) 
+                VALUES ( :rol );';
 
             $params = [':rol' => $rol,];
 
-            $this->do_query($insert_rol_query, $params);
-
-
+            $this->do_query($update_rol_query, $params);
         }
+
 
         function read(){
             $get_user_query = 'SELECT * FROM rol';
             $this->get_query($get_user_query);
 
             $result = '
+            <div class="d-flex justify-content-end mb-3">
+                <form method="POST">
+                    <button class="btn btn-success btn-sm" title="Agregar nuevo rol">
+                        <i class="bi bi-plus-lg"></i> Agregar Rol
+                    </button>
+                    <input type="hidden" name="action" value="formNew">
+                </form>
+            </div>
+
             <div class="table-responsive">
             <table class="table table-striped table-hover shadow-sm">
                 <thead class="table-primary text-center">
@@ -62,8 +111,15 @@
                         <td class='text-center'> ".$register['rol']." </td>
                         <td class='text-center'>
                             <div class='d-flex justify-content-center gap-2'>
-                                <button class='btn btn-primary'><img class='svg-white' style='width: 25px;' src='../assets/icons/edit-button.svg' alt='Edit icon' srcset=''></button>
-                                <button class='btn btn-danger'><img class='svg-white' style='width: 30px;' src='../assets/icons/delete.svg' alt='Delete icon' srcset=''></button>
+                                <form class='btn btn-primary'>
+                                    <input type='image' class='svg-white' style='width: 25px;' src='../assets/icons/edit-button.svg' alt='Edit icon' srcset=''>
+                                   
+                                </form>
+                                <form method='POST' class='btn btn-danger'>
+                                    <input type='image' class='svg-white' style='width: 30px;' src='../assets/icons/delete.svg' alt='Delete icon' srcset=''>
+                                    <input type='hidden' name='action' value='delete'>
+                                    <input type='hidden' name='id_rol' value=".$register['id_rol'].">
+                                </form>
                             </div>
                         </td>
                     </tr>";
@@ -76,12 +132,13 @@
             echo $result;
         }
 
-        function update(){
-            
-        }
-
         function delete(){
-            
+            $id_rol = $_POST['id_rol'];
+            $delete_rol_query = 'DELETE FROM rol WHERE id_rol = :id_rol;';
+
+            $params = [':id_rol' => $id_rol,];
+            $this->do_query($delete_rol_query, $params);
+            $this->read();
         }
     }
 
