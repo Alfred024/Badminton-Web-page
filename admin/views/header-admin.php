ee3ec939f651e328a8a783c859bd90f957efee23
unknown <21030761@itcelaya.edu.mx>
2024-10-29 12:36:04 -0600

    Estado renta UI and class

--- admin/views/header-admin.php
+++ admin/views/header-admin.php
@@ -19,6 +19,7 @@
             <li class="nav-item"><a class="nav-link" href="./roles.php">Roles</a></li>
             <li class="nav-item"><a class="nav-link" href="./marcas.php">Marcas</a></li>
             <li class="nav-item"><a class="nav-link" href="./formas_pago.php">Formas pago</a></li>
+            <li class="nav-item"><a class="nav-link" href="./estado_renta.php">Estado renta</a></li>
             <li class="nav-item"><a class="nav-link" href="../index.php">Cerrar sesión</a></li>
         </ul>
     </nav>
